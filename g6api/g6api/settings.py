da5d86e586cd23b392ff984d61d366546d3ccd59
Belal33 <belalelbanna7@gmail.com>
2023-03-29 17:34:59 +0200

    add cors headers settings

--- g6api/g6api/settings.py
+++ g6api/g6api/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "rest_framework", 
+    "corsheaders", 
     
     "accounts.apps.AccountsConfig",
 ]
@@ -44,12 +47,22 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# new
+CORS_ORIGIN_WHITELIST = (
+    "http://localhost:3000",
+    "http://localhost:8000",
+)
+
+# frontendhost
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"] # new
 
 ROOT_URLCONF = 'g6api.urls'
 
