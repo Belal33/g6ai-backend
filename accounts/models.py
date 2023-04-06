322fd9b3a63d80c738b4f553373fd41c80e93383
Belal33 <belalelbanna7@gmail.com>
2023-04-06 03:46:40 +0200

    rename user_id to temp_id

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser 
 import uuid
 class CustomUser(AbstractUser):
-  id = models.UUIDField(max_length=36, default=uuid.uuid4, unique=True ,primary_key=True,editable=False)
+  temp_id = models.UUIDField(default=uuid.uuid4, unique=True )
 
   age = models.IntegerField(blank=True,null=True)
