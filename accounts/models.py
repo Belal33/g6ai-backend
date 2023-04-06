446d6ea843b26265d650efc7bca53f1303df96e6
Belal33 <belalelbanna7@gmail.com>
2023-04-06 12:56:59 +0200

    remove migrations but the initial one

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser 
 import uuid
 class CustomUser(AbstractUser):
-  custom_id = models.UUIDField( default=uuid.uuid4, unique=True ,primary_key=True,editable=False)
+  id = models.UUIDField( default=uuid.uuid4, unique=True ,primary_key=True,editable=False)
 
   age = models.IntegerField(blank=True,null=True)
