39c63eae08f8c4866bd58f2fadbbbfb34fda4bf8
Belal33 <belalelbanna7@gmail.com>
2023-04-26 00:28:02 +0200

    edit system message filed in chat box model

--- chatv1/consumers.py
+++ chatv1/consumers.py
@@ -1,22 +1,19 @@
 from channels.generic.websocket import JsonWebsocketConsumer
 from .chatbot_stream import get_gpt_chat_response
-from django.contrib.auth import get_user_model
 
-get_user_model().is_anonymous
-get_user_model().is_authenticated
+
 
 class TokenAuthConsumer(JsonWebsocketConsumer):
   
     def connect(self):
-      # user = self.scope.get("user")
-      # if user and user.is_authenticated and not user.is_anonymous:
-      #   self.accept()
-      # elif user.is_anonymous :
-      #   self.close(code=400)
-      # else: 
-      #   self.close(code=401)
+      user = self.scope.get("user")
+      if user and user.is_authenticated and not user.is_anonymous:
+        self.accept()
+      elif user.is_anonymous :
+        self.close(code=400)
+      else: 
+        self.close(code=401)
 
-      self.accept()
       
 
     # def disconnect(self, close_code):
