9c0dfa3aad66630d8af40c694848fd39a139b279
Belal33 <belalelbanna7@gmail.com>
2023-05-04 00:56:33 +0300

    add voice endpoint

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import requests
 from os import stat
 
 url = "http://127.0.0.1:8000/api/v1/chat/file/"
-url = "http://127.0.0.1:8000/api/v1/chat/voice/"
+# url = "http://127.0.0.1:8000/api/v1/chat/voice/"
 file_path = "AI_Modules_hameed.webm"
 
 size = stat(file_path).st_size
