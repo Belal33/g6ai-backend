dd1c488b02f4adab7a97c320f71e83b6b9bdfc06
Belal33 <belalelbanna7@gmail.com>
2023-05-05 01:52:37 +0300

    intial for production

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,42 +1,42 @@
-from django.urls import path,include,re_path
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from dj_rest_auth.views import (
-    LoginView, LogoutView, PasswordChangeView, PasswordResetConfirmView,
-    PasswordResetView, UserDetailsView,
+    LoginView,
+    LogoutView,
+    PasswordChangeView,
+    PasswordResetConfirmView,
+    PasswordResetView,
+    UserDetailsView,
 )
-from dj_rest_auth.registration.views import (SocialAccountListView,ConfirmEmailView)
+from dj_rest_auth.registration.views import SocialAccountListView, ConfirmEmailView
 
 from dj_rest_auth.jwt_auth import get_refresh_view
 from rest_framework_simplejwt.views import TokenVerifyView
 
-from .views import GoogleLogin  #, google_callback, oauth2_callback, oauth2_login
+from .views import GoogleLogin  # , google_callback, oauth2_callback, oauth2_login
+
 # from allauth.account.views import confirm_email as allauthemailconfirmation
 
 
-urlpatterns =[
-
-
-    
+urlpatterns = [
     # path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     # path('token/refresh/', get_refresh_view().as_view(), name='token_refresh'),
-    
     # for the get response => for test
     # path('password/reset/confirm/<uidb64>/<token>/',
     #   TemplateView.as_view(template_name="password_reset_confirm.html"),
     #   name='password_reset_confirm'),
-    
     # for the get response => for test
-    path('password/reset/confirm/<slug:uidb64>/<slug:token>/',
+    path(
+        "password/reset/confirm/<slug:uidb64>/<slug:token>/",
         PasswordResetConfirmView.as_view(),
-        name='password_reset_confirm',
+        name="password_reset_confirm",
     ),
-
-
-
-    path('registration/account-confirm-email/<key>/', ConfirmEmailView.as_view(), name='account_email_verification_sent'),
-
-
-    path('', include('dj_rest_auth.urls')),
+    path(
+        "registration/account-confirm-email/<key>/",
+        ConfirmEmailView.as_view(),
+        name="account_email_verification_sent",
+    ),
+    path("", include("dj_rest_auth.urls")),
     # URLs that do not require a session or valid token
     # path('password/reset/', PasswordResetView.as_view(), name='rest_password_reset'),
     # path('password/reset/confirm/', PasswordResetConfirmView.as_view(), name='rest_password_reset_confirm'),
@@ -45,18 +45,12 @@ urlpatterns =[
     # path('logout/', LogoutView.as_view(), name='rest_logout'),
     # path('user/', UserDetailsView.as_view(), name='rest_user_details'),
     # path('password/change/', PasswordChangeView.as_view(), name='rest_password_change'),
-
-    
-    path('registration/', include('dj_rest_auth.registration.urls')),
-
-    path('socialaccounts/', SocialAccountListView.as_view(), name='social_account_list'),
-
-    path('google/', GoogleLogin.as_view(), name='google_login'),
+    path("registration/", include("dj_rest_auth.registration.urls")),
+    # path('socialaccounts/', SocialAccountListView.as_view(), name='social_account_list'),
+    path("google/", GoogleLogin.as_view(), name="google_login"),
     # path('', include('allauth.socialaccount.providers.google.urls')),
-        # "google/login/"
-        # "google/login/callback/
-        
-    
+    # "google/login/"
+    # "google/login/callback/
 ]
 # api/v1/auth/ password/reset/ [name='rest_password_reset']
 # api/v1/auth/ password/reset/confirm/ [name='rest_password_reset_confirm']
