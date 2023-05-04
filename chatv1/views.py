f5eb0958077cc2306c367a4222eb4395d03713ba
Belal33 <belalelbanna7@gmail.com>
2023-05-04 20:08:32 +0300

    add voice endpoint

--- chatv1/views.py
+++ chatv1/views.py
@@ -162,7 +162,7 @@ class FileUploadView(CreateAPIView):
                             {"error": str(e)},
                             status=status.HTTP_500_INTERNAL_SERVER_ERROR,
                         )
-                    f = file.file
+                    f = file
                     base64data = f.split(",")[1]
 
                     decoded_data = base64.b64decode(base64data)
